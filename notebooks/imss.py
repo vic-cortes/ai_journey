73fe8728fde586f5a6b9fb76a3156e9c48a91a30
Victor Cortes Figueroa <vmcortesf@gmail.com>
2025-03-16 08:07:36 -0500

    Refactor imss.py: change app width to full, add new cells for data handling and documentation

--- notebooks/imss.py
+++ notebooks/imss.py
@@ -1,11 +1,11 @@
 import marimo
 
 __generated_with = "0.11.17"
-app = marimo.App(width="medium")
+app = marimo.App(width="full")
 
 
 @app.cell
-def _():
+def libraries():
     from pathlib import Path
 
     import polars as pl
@@ -22,8 +22,36 @@ def _(Path):
 
 @app.cell
 def _(filename, pl):
-    df = pl.read_csv(filename, separator="|")
+    df = pl.read_csv(
+        filename,
+        separator="|",
+    )
     return (df,)
+
+
+@app.cell
+def _(df):
+    df.head()
+    df.columns
+    return
+
+
+@app.cell
+def _():
+    import marimo as mo
+    return (mo,)
+
+
+@app.cell
+def _(mo):
+    mo.md(
+        r"""
+        # Busqueda
+
+        Es importanre filtrar la información por entidad, y municipio. Es necesario hacer uso de los datos abiertos del INEGI
+        """
+    )
+    return
 
 
 if __name__ == "__main__":
